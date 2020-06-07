07816cca4cfd197174c582696231b997b4aebb12
Menno <menno@mmerlijn.nl>
2020-06-07 12:48:59 +0200

    Fix hele dag en wisbutton

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -10,8 +10,8 @@
                        @change="tot=toTime(tot)" tabindex="2"></input>
             <button class="bg-primary hover:bg-primary-dark font-bold text-white p-2 rounded focus:shadow-outline" @click.prevent="add" tabindex="3"> + </button>
             <button class="bg-primary hover:bg-primary-dark font-bold text-white p-2 rounded focus:shadow-outline" @click.prevent="sub" tabindex="4"> - </button>
-            <button class="bg-primary hover:bg-primary-dark font-bold text-white p-2 rounded focus:shadow-outline" @click.prevent="[van='0:00',tot='23:59',add]" tabindex="5">Hele dag</button>
-            <button class="bg-primary hover:bg-primary-dark font-bold text-white p-2 rounded focus:shadow-outline" @click.prevent="this.value=[]" tabindex="6">Wis</button>
+            <button class="bg-primary hover:bg-primary-dark font-bold text-white p-2 rounded focus:shadow-outline" @click.prevent="day" tabindex="5">Hele dag</button>
+            <button class="bg-primary hover:bg-primary-dark font-bold text-white p-2 rounded focus:shadow-outline" @click.prevent="trash" tabindex="6">Wis</button>
             <p class="p-4">Op tijden: {{timeArray.humanReadable()}}</p>
         </template>
     </default-field>
@@ -32,6 +32,20 @@ export default {
             timeArray: new TimeArray(),
         }
     },
+    watch:{
+        min: {
+            handler: function (val, oldVal) {
+                this.countFactor();
+            },
+            deep: true
+        },
+        timeArray:{
+            handler: function (val,oldVal) {
+                this.value = this.timeArray.array;
+            },
+            deep: true
+        }
+    },
     methods: {
         toTime(value) {
             value = value.replace(/^[0]?/, '');
@@ -49,16 +63,26 @@ export default {
         time2int(val){
             return  parseInt(val.split(':')[0])*60+parseInt(val.split(':')[1]);
         },
+        trash(){
+          this.value=[];
+          this.timeArray.array=[];
+        },
+        day(){
+            this.timeArray.add([0,1439]);
+            this.van='';
+            this.tot='';
+            this.$refs.van.focus();
+        },
         add(){
             this.timeArray.add([this.time2int(this.van),this.time2int(this.tot)]);
-            this.value = this.timeArray.array;
+            //this.value = this.timeArray.array;
             this.van='';
             this.tot='';
             this.$refs.van.focus();
         },
         sub(){
             this.timeArray.sub([this.time2int(this.van),this.time2int(this.tot)]);
-            this.value = this.timeArray.array;
+            //this.value = this.timeArray.array;
             this.van='';
             this.tot='';
             this.$refs.van.focus();
@@ -68,7 +92,7 @@ export default {
          */
         setInitialValue() {
             this.timeArray.add(this.field.value || []);
-            this.value = this.timeArray.array;
+            //this.value = this.timeArray.array;
         },
 
         /**
